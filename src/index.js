576f5efc015726f31465f1fe1662bfa0f0909401
Alexey Kudryavcev <ackudryavcev@gmail.com>
2020-03-28 19:22:31 +0300

    corrected version

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import './pages/style.css';
+
+const buttonMobileMenu = document.querySelector('.header__menu-mobile');
+const crossMobileMenu = document.querySelector('.popup__menu-close');
+
+function openMobileMenu() {
+  document.querySelector('.popup__menu').classList.toggle('popup__menu_is-opened');
+}
+
+function closeMobileMenu() {
+  document.querySelector('.popup__menu').classList.toggle('popup__menu_is-opened');
+}
+
+buttonMobileMenu.addEventListener('click', openMobileMenu);
+crossMobileMenu.addEventListener('click', closeMobileMenu);
