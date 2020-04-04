9ccdab9774a0782cede35ae296beae20d95fc542
Alexey Kudryavcev <ackudryavcev@gmail.com>
2020-04-04 22:28:46 +0300

    corrected version

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable indent */
+/* eslint-disable eol-last */
 /* eslint-disable no-undef */
+
 import './pages/style.css';
 
 const buttonMobileMenu = document.querySelector('.header__menu-mobile');
 const crossMobileMenu = document.querySelector('.popup__menu-close');
 
 function openMobileMenu() {
-  document.querySelector('.popup__menu').classList.toggle('popup__menu_is-opened');
+    document.querySelector('.popup__menu').classList.toggle('popup__menu_is-opened');
 }
 
 function closeMobileMenu() {
-  document.querySelector('.popup__menu').classList.toggle('popup__menu_is-opened');
+    document.querySelector('.popup__menu').classList.toggle('popup__menu_is-opened');
 }
+
 
 buttonMobileMenu.addEventListener('click', openMobileMenu);
 crossMobileMenu.addEventListener('click', closeMobileMenu);
